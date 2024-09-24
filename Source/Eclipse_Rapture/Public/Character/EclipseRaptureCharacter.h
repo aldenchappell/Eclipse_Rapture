7b0aacb5a87bdd54ab32da8c4870bc257e5b4197
Alden Chappell <118118263+aldenchappell@users.noreply.github.com>
2024-09-24 12:53:15 -0400

    Player Input actions setup with code

---
+++ Source/Eclipse_Rapture/Public/Character/EclipseRaptureCharacter.h
@@ -0,0 +1,77 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "InputActionValue.h"
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "EclipseRaptureCharacter.generated.h"
+
+//Forward Declarations
+class UInputMappingContext;
+class UInputAction;
+class UCameraComponent;
+UCLASS()
+class ECLIPSE_RAPTURE_API AEclipseRaptureCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+#pragma region Setup
+	AEclipseRaptureCharacter();
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+#pragma endregion
+
+	
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void Jump() override;
+#pragma region Input Actions
+
+	UPROPERTY(EditAnywhere, Category = Input)
+	UInputMappingContext* EclipseRaptureMapping;
+
+	UPROPERTY(EditAnywhere, Category = Input)
+	UInputAction* MovementAction;
+
+	UPROPERTY(EditAnywhere, Category = Input)
+	UInputAction* LookAction;
+
+	UPROPERTY(EditAnywhere, Category = Input)
+	UInputAction* InteractAction;
+
+	UPROPERTY(EditAnywhere, Category = Input)
+	UInputAction* JumpAction;
+
+	UPROPERTY(EditAnywhere, Category = Input)
+	UInputAction* CrouchAction;
+
+	UPROPERTY(EditAnywhere, Category = Input)
+	UInputAction* ProneAction;
+
+	UPROPERTY(EditAnywhere, Category = Input)
+	UInputAction* ShootAction;
+
+	UPROPERTY(EditAnywhere, Category = Input)
+	UInputAction* AimAction;
+
+	UPROPERTY(EditAnywhere, Category = Input)
+	UInputAction* MeleeAction;
+#pragma endregion
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+
+
+	void Interact();
+	void Shoot();
+	void Melee();
+	void Aim();
+	void Prone();
+	void Crouch();
+public:	
+	
+	
+
+};
