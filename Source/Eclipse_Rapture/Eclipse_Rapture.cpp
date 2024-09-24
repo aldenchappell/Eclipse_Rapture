d5a4e555bd13ae48588bd34505267b776545f876
Alden Chappell <118118263+aldenchappell@users.noreply.github.com>
2024-09-24 12:53:15 -0400

    Player Input actions setup with code

---
+++ Source/Eclipse_Rapture/Eclipse_Rapture.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Eclipse_Rapture.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Eclipse_Rapture, "Eclipse_Rapture" );
