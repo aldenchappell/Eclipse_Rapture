c7d11c0626fca5b7a7984b1abc92f93d1682597e
Alden Chappell <118118263+aldenchappell@users.noreply.github.com>
2024-10-16 16:33:32 -0400

    awd

---
+++ Source/Eclipse_Rapture/Private/Interfaces/CharacterData.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interfaces/CharacterData.h"
+
+// Add default functionality here for any ICharacterData functions that are not pure virtual.
