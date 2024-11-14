2dc343c3f4292c928ff7282f69f9b5e7e875e0e6
Alden Chappell <118118263+aldenchappell@users.noreply.github.com>
2024-11-14 16:31:41 -0500

    Enemy AI character
    Importing model/anims for an enemy model for testing purposes.

--- Source/Eclipse_Rapture/Private/Enemies/DespawningComponent.cpp
+++ Source/Eclipse_Rapture/Private/Enemies/DespawningComponent.cpp
@@ -1,58 +1,82 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// DespawningComponent.cpp
 
+#include "Enemies/DespawningComponent.h"
 #include "Components/SphereComponent.h"
 #include "Kismet/GameplayStatics.h"
-#include "Enemies/DespawningComponent.h"
+#include "TimerManager.h"
+
 
 UDespawningComponent::UDespawningComponent()
 {
-	PrimaryComponentTick.bCanEverTick = false;
+    PrimaryComponentTick.bCanEverTick = false;
 
-	DespawnSphere = CreateDefaultSubobject<USphereComponent>(TEXT("DespawnSphere"));
-	DespawnSphere->SetSphereRadius(DespawnRadius);
+    // Initialize and set up DespawnSphere
+    DespawnSphere = CreateDefaultSubobject<USphereComponent>(TEXT("DespawnSphere"));
+    DespawnSphere->SetSphereRadius(DespawnRadius);
 }
 
 void UDespawningComponent::BeginPlay()
 {
-	Super::BeginPlay();
-	
+    Super::BeginPlay();
+
+    // Register overlap events
+    DespawnSphere->OnComponentBeginOverlap.AddDynamic(this, &UDespawningComponent::OnSphereOverlap);
+    DespawnSphere->OnComponentEndOverlap.AddDynamic(this, &UDespawningComponent::OnSphereEndOverlap);
 }
 
 void UDespawningComponent::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if (bEnableDespawnRadius)
-	{
-		bShouldDespawn = false;
-	}
+    if (bEnableDespawnRadius)
+    {
+        bShouldDespawn = false;
+    }
 }
 
 void UDespawningComponent::OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
 {
-	if (bEnableDespawnRadius)
-	{
-		bShouldDespawn = true;
-	}
+    if (bEnableDespawnRadius)
+    {
+        bShouldDespawn = true;
+        CheckDespawn();
+    }
+}
+
+void UDespawningComponent::StartDespawnTimer()
+{
+    // Start the timer-based despawn only after character death only if despawn radius isn't enabled.
+    if (!bEnableDespawnRadius)
+    {
+        GetWorld()->GetTimerManager().SetTimer(
+            DespawnTimerHandle,
+            this,
+            &UDespawningComponent::CheckDespawn,
+            DespawnDelay,
+            false);
+    }
+    else
+    {
+        UE_LOG(LogTemp, Warning, TEXT("Despawn timer not started. Despawn radius is enabled."));
+    }
 }
 
 void UDespawningComponent::CheckDespawn()
 {
-	if (!bShouldDespawn) return;
+    if (bEnableDespawnRadius)
+    {
+        // If using radius-based despawning, check player distance
+        if (APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0))
+        {
+            float DistanceToPlayer = FVector::Dist(GetOwner()->GetActorLocation(), PlayerPawn->GetActorLocation());
 
-	if (APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0))
-	{
-		float DistanceToPlayer = FVector::Dist(GetOwner()->GetActorLocation(), PlayerPawn->GetActorLocation());
-
-		if (bEnableDespawnRadius)
-		{
-			if (DistanceToPlayer > DespawnSphere->GetScaledSphereRadius())
-			{
-				GetOwner()->Destroy();
-			}
-		}
-		else
-		{
-			GetOwner()->Destroy();
-		}
-	}
+            if (DistanceToPlayer > DespawnSphere->GetScaledSphereRadius())
+            {
+                GetOwner()->Destroy();
+            }
+        }
+    }
+    else
+    {
+        // Timer-based despawn
+        GetOwner()->Destroy();
+    }
 }
-
--- Source/Eclipse_Rapture/Public/Enemies/DespawningComponent.h
+++ Source/Eclipse_Rapture/Public/Enemies/DespawningComponent.h
@@ -1,43 +1,48 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// DespawningComponent.h
 
 #pragma once
+
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
 #include "DespawningComponent.generated.h"
 
-
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class ECLIPSE_RAPTURE_API UDespawningComponent : public UActorComponent
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
-public:	
-	UDespawningComponent();
+public:
+    UDespawningComponent();
 
-	UPROPERTY(EditAnywhere, BlueprintReadonly, Category = "Despawning")
-	bool bEnableDespawnRadius = true;
+    UPROPERTY(EditAnywhere, BlueprintReadonly, Category = "Despawning")
+    bool bEnableDespawnRadius = true;
 
-	bool bShouldDespawn = false;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Despawning")
+    float DespawnDelay = 5.0f;  // Time in seconds before destruction after death
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Despawning")
-	TObjectPtr<class USphereComponent> DespawnSphere;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Despawning")
+    TObjectPtr<class USphereComponent> DespawnSphere;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Despawning")
-	float DespawnRadius = 5000.f;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Despawning")
+    float DespawnRadius = 5000.f;
 
-	UFUNCTION(Blueprintcallable)
-	void CheckDespawn();
+	//Called upon death to start the despawn timer
+    UFUNCTION(BlueprintCallable, Category = "Despawning")
+    void StartDespawnTimer();
 
 protected:
-	
-	virtual void BeginPlay() override;
+    virtual void BeginPlay() override;
 
-	UFUNCTION()
-	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+    UFUNCTION()
+    virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
-	UFUNCTION()
-	virtual void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+    UFUNCTION()
+    virtual void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 
-		
+private:
+    bool bShouldDespawn = false;
+    FTimerHandle DespawnTimerHandle;
+
+    void CheckDespawn();
 };
