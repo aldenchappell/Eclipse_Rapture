dc5c27aa3046ef5bb989e3655a7db9f28acde77a
Alden Chappell <118118263+aldenchappell@users.noreply.github.com>
2024-12-31 16:46:09 -0500

    Inventory
    About to make a big overhaul on logic

--- Source/Eclipse_Rapture/Public/Structures/FInventoryTypes.h
+++ Source/Eclipse_Rapture/Public/Structures/FInventoryTypes.h
@@ -1,7 +1,10 @@
 #pragma once
 
+#include "Items/ItemTypes.h"
 #include "CoreMinimal.h"
 #include "FInventoryTypes.generated.h"
+
+
 
 USTRUCT(BlueprintType)
 struct FInventorySpaceRequirements
@@ -26,4 +29,41 @@ struct FInventoryTile
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Inventory Tile")
     FVector2D YTiling;
+};
+
+USTRUCT(BlueprintType)
+struct FItemData
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item Data")
+    EItemType ItemType;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item Data")
+    EUsecaseType UsecaseType;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item Data")
+    bool bDestroyOnPickup;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item Data")
+    FInventorySpaceRequirements InventorySpaceRequired;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item Data")
+    EUseImpactType UseImpactType;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item Data")
+    EMainItemUseDescriptionType MainItemUseDescriptionType;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item Data")
+    ESecondaryItemUseDescriptionType SecondaryItemUseDescriptionType;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item Data")
+    FText UseActionText;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item Data")
+    UMaterialInterface* ItemIcon;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item Data")
+    UMaterialInterface* ItemIconRotated;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item Data")
+    FText ItemDisplayName;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item Data")
+    FText ItemDescription;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item Data")
+    float ItemWeight;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item Data")
+    int32 MaxStackSize;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item Data")
+    FText ItemInteractionPrompt;
 };
